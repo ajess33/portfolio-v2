7a40a4baf0436b87fce7000e53774b093398951a
Austin Jess <ajess33@gmail.com>
2019-02-04 22:09:25 -0600

    added header

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,17 +28,17 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
         </div>
       </>
     )}
   />
 );
 
+// <footer>
+//   © {new Date().getFullYear()}, Built with
+//   {` `}
+//   <a href="https://www.gatsbyjs.org">Gatsby</a>
+// </footer>
 Layout.propTypes = {
   children: PropTypes.node.isRequired
 };
