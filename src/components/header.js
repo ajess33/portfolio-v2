290aff281f9421cec88b210aa1a4aae2090c48c5
Austin Jess <ajess33@gmail.com>
2019-02-15 18:58:37 -0600

    created responsive header

--- src/components/header.js
+++ src/components/header.js
@@ -6,30 +6,68 @@ import { IoMdMenu } from 'react-icons/io';
 
 // STILL NEED TO MAKE RESPONSIVE
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <img src={headshot} alt="Headshot of Austin Jess" />
-    <nav>
-      <h1>
-        <Link to="/">{siteTitle}</Link>
-      </h1>
-      <div className="menu-mobile">
-        <IoMdMenu size={35} />
-      </div>
-      <ul>
-        <li>
-          <Link to="/Portfolio/">PORTFOLIO</Link>
-        </li>
-        <li>
-          <Link to="/Toolbox/">TOOLBOX</Link>
-        </li>{' '}
-        <li>
-          <Link to="/Contact/">CONTACT</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+// const Header = ({ siteTitle }) => (
+//   <header>
+//     <img src={headshot} alt="Headshot of Austin Jess" />
+//     <nav>
+//       <h1>
+//         <Link to="/">{siteTitle}</Link>
+//       </h1>
+//       <div className="menu-mobile">
+//         <IoMdMenu size={35} />
+//       </div>
+//       <ul className="menu-full">
+//         <li>
+//           <Link to="/Portfolio/">PORTFOLIO</Link>
+//         </li>
+//         <li>
+//           <Link to="/Toolbox/">TOOLBOX</Link>
+//         </li>{' '}
+//         <li>
+//           <Link to="/Contact/">CONTACT</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   </header>
+// );
+
+class Header extends React.Component {
+  componentDidMount() {
+    let menuFull = document.getElementById('js-main-nav');
+    let menuMobile = document.getElementById('js-menu-mobile');
+
+    menuMobile.addEventListener('click', () => {
+      menuFull.classList.toggle('active');
+    });
+  }
+
+  render() {
+    return (
+      <header>
+        <img src={headshot} alt="Headshot of Austin Jess" />
+        <nav>
+          <h1>
+            <Link to="/">Austin Jess</Link>
+          </h1>
+          <div id="js-menu-mobile" className="menu-mobile">
+            <IoMdMenu size={35} />
+          </div>
+          <ul id="js-main-nav">
+            <li>
+              <Link to="/Portfolio/">PORTFOLIO</Link>
+            </li>
+            <li>
+              <Link to="/Toolbox/">TOOLBOX</Link>
+            </li>{' '}
+            <li>
+              <Link to="/Contact/">CONTACT</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -66,8 +66,8 @@ const AcgProject = () => (
         </p>
       </div>
       <div className="buttons">
-        <a href="https://ajess33.github.io/dunder-mifflinipsum/">LIVE</a>
-        <a href="https://github.com/ajess33/dunder-mifflinipsum">CODE</a>
+        <a href="https://acleangetawayllc.com/">LIVE</a>
+        <a href="https://github.com/ajess33/A-Clean-Getaway-Website">CODE</a>
       </div>
     </div>
     <AcgImage className="project-image" />
@@ -91,87 +91,10 @@ const PomoProject = () => (
         </p>
       </div>
       <div className="buttons">
-        <a href="https://ajess33.github.io/dunder-mifflinipsum/">LIVE</a>
-        <a href="https://github.com/ajess33/dunder-mifflinipsum">CODE</a>
+        <a href="https://ajess33.github.io/Pomodoro-Clock/">LIVE</a>
+        <a href="https://github.com/ajess33/Pomodoro-Clock">CODE</a>
       </div>
     </div>
     <PomoImage className="project-image" />
   </div>
 );
-
-// export const query = graphql`
-//   query {
-//     dunderImage: file(relativePath: { eq: "dunder-500.png" }) {
-//       childImageSharp {
-//         fixed(width: 300) {
-//           ...GatsbyImageSharpFixed
-//         }
-//       }
-//     }
-
-//     acgImage: file(relativePath: { eq: "acg-500.png" }) {
-//       childImageSharp {
-//         fixed(width: 300) {
-//           ...GatsbyImageSharpFixed
-//         }
-//       }
-//     }
-
-//     pomoImage: file(relativePath: { eq: "pomo-500.png" }) {
-//       childImageSharp {
-//         fixed(width: 300) {
-//           ...GatsbyImageSharpFixed
-//         }
-//       }
-//     }
-//   }
-// `;
-
-// <div className="project-flex">
-//   <div>
-//     <h3>Dunder MifflinIpsum</h3>
-//     <hr />
-//     <p>
-//       Michael and his jeans... he gets in them, and I'm not exactly sure
-//       what happens, but I can tell you he loves the way he looks in those
-//       jeans. I know that's why he started casual Fridays.
-//     </p>
-//     <p>
-//       I wish there was a way to know you're in the good old days before
-//       you've actually left them.
-//     </p>
-//   </div>
-//   <DunderImage className="project-image" />
-// </div>
-// <div className="project-flex">
-//   <div>
-//     <h3>A Clean Getaway Website</h3>
-//     <hr />
-//     <p>
-//       Michael and his jeans... he gets in them, and I'm not exactly sure
-//       what happens, but I can tell you he loves the way he looks in those
-//       jeans. I know that's why he started casual Fridays.
-//     </p>
-//     <p>
-//       I wish there was a way to know you're in the good old days before
-//       you've actually left them.
-//     </p>
-//   </div>
-//   <AcgImage className="project-image" />
-// </div>
-// <div className="project-flex">
-//   <div>
-//     <h3>Pomodoro Clock</h3>
-//     <hr />
-//     <p>
-//       Michael and his jeans... he gets in them, and I'm not exactly sure
-//       what happens, but I can tell you he loves the way he looks in those
-//       jeans. I know that's why he started casual Fridays.
-//     </p>
-//     <p>
-//       I wish there was a way to know you're in the good old days before
-//       you've actually left them.
-//     </p>
-//   </div>
-//   <PomoImage className="project-image" />
-// </div>
